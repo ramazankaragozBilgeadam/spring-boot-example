1a1fe8055e1f28f19a30d3ccfe049c8432306319
ramazan.karagoz <ramazan.karagoz.official@gmail.com>
2019-05-25 12:27:45 +0300

    add

--- src/main/java/com/bilgeadam/example/springboot/controller/HomeController.java
+++ src/main/java/com/bilgeadam/example/springboot/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.bilgeadam.example.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +11,17 @@ import java.util.Date;
 public class HomeController {
 
 
+    @Value(value = "${server.port}")
+    private String portInfo;
+
     //@RequestMapping(value = "/home",method = RequestMethod.GET)
     @GetMapping("/home")
     public String home(Model model){
 
         model.addAttribute("serverDate",new Date());
+        model.addAttribute("portInfo",portInfo);
         return "home";
     }
+
 
 }
